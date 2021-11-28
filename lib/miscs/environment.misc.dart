190c575c039ff3e8f2130023df67188313c27c44
TNBruh <siondoll66@gmail.com>
2021-11-28 14:16:01 +0800

    godihopethisisthefinalcommitorimgoingtocommitwarcrimesandkillmyself

--- lib/miscs/environment.misc.dart
+++ lib/miscs/environment.misc.dart
@@ -5,7 +5,6 @@ class Environment {
   static const int hosting = 5000;
   static const int pubsub = 8085; //not actually used
   static const int storage = 9199; //not actually used
-  static const String broadcastUrl =
-      'http://10.0.2.2:5001/fractal-unj-9f50c/us-central1/amongus/broadcast';
+  static const String broadcastUrl = 'http://13.54.202.79:1337/amogus';
   //http://localhost:5001/fractal-unj-9f50c/us-central1/broadcast
 }
